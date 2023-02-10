44fbbb48fe459b05484a311202941c1909a7800d
aleksas <aasmc@yandex.ru>
2023-02-10 15:19:22 +0300

    Initial commit

---
+++ src/test/java/ru/aasmc/efficientdatajpatesting/EfficientDataJpaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aasmc.efficientdatajpatesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfficientDataJpaTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
